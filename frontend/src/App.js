43b3169dd1163677efd09fa6f62b917adde3fc10
Chimeruzee <68004888+Ehceyn@users.noreply.github.com>
2022-01-04 21:28:33 +0100

    Small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,23 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Header from "./Components/Header"
-import Home from "./Components/Home"
-import   Checkout from "./Components/Checkout";
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Checkout from "./Components/Checkout";
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path='/checkout'>
-            <Header/>
-            <Checkout/>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
           </Route>
-          <Route path='/login'>
-            {/* <Login/> */}
+          <Route path="/login">{/* <Login/> */}</Route>
+          <Route path="/">
+            <Header />
+            <Home />
           </Route>
-          <Route path='/'>
-            <Header/>
-            <Home/>
-          </Route> 
         </Switch>
       </div>
     </Router>
